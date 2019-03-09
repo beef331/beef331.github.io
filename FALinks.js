c9bff0a44b1bba4288bba24b25519a11132bfbd8
Jason Beetham <beefers331@gmail.com>
2019-03-09 00:47:45 -0700

    Added footer

---
+++ FALinks.js
@@ -0,0 +1,5 @@
+let faLinks ={
+    "fas fa-envelope-open" : "mailto://me@jasonbeetham.com",
+    "fab fa-github" : "https://github.com/beef331",
+    "fab fa-twitter":"https://twitter.com/Beefers331"
+}
